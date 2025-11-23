6db97653284de358207f177d0610ae0c61f9e786
Max Netyaga <max.netyaga@gmail.com>
2025-11-24 00:50:40 +0200

    Add modified_at

---
+++ astro/src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+  en: "English",
+  uk: "Українська",
+};
+
+export const defaultLang = "uk";
+
+export const ui = {
+  en: {
+    "layout.page_modified_at": "Modified at",
+  },
+  uk: {
+    "layout.page_modified_at": "Дата оновлення",
+  },
+} as const;
--- astro/src/utils.ts
+++ astro/src/utils.ts
@@ -1,10 +1,37 @@
 import { execSync } from "child_process";
+import fs from "node:fs";
 
-export function reportModifiedTime(filepath: string | null) {
+/**
+ * @param {string} page - /path/to/some/page/
+ */
+export function reportPageModifiedTime(page: string) {
+  let gitRoot;
   try {
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
-    return result.toString();
+    gitRoot = execSync("git rev-parse --show-toplevel").toString().trim();
   } catch (err) {
-    return null;
+    throw new Error("Git repository required", { cause: err });
+  }
+
+  const simplePagePath =
+    gitRoot +
+    "/astro/src/pages/" +
+    (page === "/" ? "index" : page.replace(/^\/+|\/+$/g, "")) +
+    ".astro";
+  const isSimplePage = fs.existsSync(simplePagePath);
+
+  if (!isSimplePage) {
+    throw new Error(
+      `Can't report modified time for any pages but simple (single .astro file)\nPage: ${simplePagePath}`,
+    );
+  }
+
+  const filePath = simplePagePath;
+
+  const errorMsg = "file wasn't commited yet";
+  try {
+    const result = execSync(`git log -1 --pretty="format:%cI" "${filePath}"`);
+    return result.toString() || errorMsg;
+  } catch {
+    return errorMsg;
   }
 }
